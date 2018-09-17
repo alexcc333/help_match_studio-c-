089616e5520c197abbd529cc056d725267d3cc8b
alexcc333 <36711665+alexcc333@users.noreply.github.com>
2018-09-17 21:11:57 +0300

    fix case switch

--- main.cpp
+++ main.cpp
@@ -6,10 +6,21 @@ using namespace std;
 
 int main()
 {
-   /*root kvadrat;
-   kvadrat.root_resultat();
     sqare koren;
-    koren.sqare_resultat();*/
-    VECTOR vector_3;
-    vector_3.vector_1();
+    root kvadrat;
+
+    int i_1;
+    cin>>i_1;
+    switch (i_1)
+    {
+    case 1:
+        kvadrat.root_resultat();
+        break;
+     case 2:
+        koren.sqare_resultat();
+        break;
+    }
+
+
+
 }
